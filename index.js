c8db603cae912f7888e70676696c6b69500c1333
rodrigopieritz <rodrigopieritz@gmail.com>
2023-10-12 19:25:07 -0300

    rota post no index.js

--- index.js
+++ index.js
@@ -1,18 +1,26 @@
-require('dotenv').config()
-const express = require('express')
-const cors = require('cors');
-const connection = require('./src/database')
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const connection = require("./src/database");
 
-const app = express()
-app.use(express.json())
-app.use(cors({
-    origin: '*',
+const createPatient = require("./src/controllers/createPatients");
+const validatePatientRequest = require("./src/middlewares/validate-patient-request");
+
+const app = express();
+app.use(express.json());
+app.use(
+  cors({
+    origin: "*",
     credentials: true,
-    allowedHeaders: ['Content-Type', 'Authorization'] 
-  }));
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
+app.post("/api/pacientes", validatePatientRequest, createPatient);
+
 app.listen(process.env.SERVER_PORT, () => {
   console.log("local server online");
-})
+});
 
 connection.authenticate();
 connection.sync({ alter: true });
