b60ca4349ac8176601618cc22e98e03280301e04
biachristie <beatrizchristie@yahoo.com.br>
2023-10-12 19:22:42 -0300

    add newAppointment to createAppointment

--- src/controllers/appointment/createAppointment.js
+++ src/controllers/appointment/createAppointment.js
@@ -1,6 +1,30 @@
+const Patient = require('../../models/patient');
+const Doctor = require('../../models/doctor');
+const Appointment = require('../../models/appointment');
+
 async function createAppointment (req, res) {
     try {
-        
+        const patient = await Patient.findByPk(req.body.id_paciente);
+        const doctor = await Doctor.findByPk(req.body.id_medico);
+
+        const appointment = {
+            id_paciente: patient.id,
+            id_medico: doctor.id,
+            motivo_consulta: req.body.motivo_consulta,
+            data_consulta: req.body.data_consulta,
+            hora_consulta: req.body.hora_consulta,
+            descricao_problema: req.body.descricao_problema,
+            medicacao_receitada: req.body.medicacao_receitada,
+            dosagem_precaucoes: req.body.dosagem_precaucoes,
+            status: req.body.status
+        };
+
+        const newAppointment = await Appointment.create(appointment);
+        return res.status(201).json({
+            id: newAppointment.id,
+            appointment
+        });
+    
     } catch (error) {
         return res.status(500).json({ message: 'Cannot perform requested requisition' });
     }
