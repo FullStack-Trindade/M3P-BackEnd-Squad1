cc14f141bd7e28752a2b97bbcfe2f2f84523819f
biachristie <beatrizchristie@yahoo.com.br>
2023-10-12 19:05:49 -0300

    add attributes to appointment model

--- src/models/appointment.js
+++ src/models/appointment.js
@@ -1,5 +1,62 @@
 const connection = require('./../database/index');
+const { Sequelize } = require('sequelize');
 
-const Appointment = connection.define('appointment', {});
+const Appointment = connection.define('appointment', {
+    id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        unique: true,
+        primaryKey: true,
+        onUpdate: 'CASCADE'
+    },
+    id_paciente: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        onUpdate: 'CASCADE'
+    },
+    id_medico: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        onUpdate: 'CASCADE'
+    },
+    motivo_consulta: {
+        type: Sequelize.STRING(68),
+        allowNull: false,
+        notEmpty: true
+    },
+    data_consulta: {
+        type: Sequelize.DATEONLY,
+        allowNull: false,
+        notEmpty: true
+    },
+    hora_consulta: {
+        type: Sequelize.DATE,
+        get: function() {
+            return this.getDataValue('hora_consulta');
+        },
+        allowNull: false,
+        notEmpty: true
+    },
+    descricao_problema: {
+        type: Sequelize.STRING(1024),
+        allowNull: false,
+        notEmpty: true
+    },
+    medicacao_receitada: {
+        type: Sequelize.STRING(1024),
+        allowNull: true
+    },
+    dosagem_precaucoes: {
+        type: Sequelize.STRING(256),
+        allowNull: false,
+        notEmpty: true
+    },
+    status: {
+        type: Sequelize.BOOLEAN,
+        allowNull: false,
+        notEmpty: true
+    }
+});
 
 module.exports = Appointment;
