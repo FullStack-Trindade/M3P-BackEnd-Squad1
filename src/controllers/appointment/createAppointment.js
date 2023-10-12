d82a8efb9c14cea9b8f31b5f3ac215b01972dc9b
biachristie <beatrizchristie@yahoo.com.br>
2023-10-12 19:11:49 -0300

    add error status to createAppointment

--- src/controllers/appointment/createAppointment.js
+++ src/controllers/appointment/createAppointment.js
@@ -1,5 +1,9 @@
 async function createAppointment (req, res) {
-
+    try {
+        
+    } catch (error) {
+        return res.status(500).json({ message: 'Cannot perform requested requisition' });
+    }
 }
 
 module.exports = createAppointment;
