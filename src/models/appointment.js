7ea4e02a7750121d7c79790b58a27a1b446c56e3
biachristie <beatrizchristie@yahoo.com.br>
2023-10-13 14:10:04 -0300

    type modification and added default values in appointment model

--- src/models/appointment.js
+++ src/models/appointment.js
@@ -1,7 +1,7 @@
 const connection = require('./../database/index');
 const { Sequelize } = require('sequelize');
 
-const Appointment = connection.define('appointment', {
+const Appointment = connection.define('consulta', {
     id: {
         type: Sequelize.INTEGER,
         autoIncrement: true,
@@ -10,16 +10,16 @@ const Appointment = connection.define('appointment', {
         primaryKey: true,
         onUpdate: 'CASCADE'
     },
-    id_paciente: {
-        type: Sequelize.INTEGER,
-        allowNull: false,
-        onUpdate: 'CASCADE'
-    },
-    id_medico: {
-        type: Sequelize.INTEGER,
-        allowNull: false,
-        onUpdate: 'CASCADE'
-    },
+    // id_paciente: {
+    //     type: Sequelize.INTEGER,
+    //     allowNull: false,
+    //     onUpdate: 'CASCADE'
+    // },
+    // id_medico: {
+    //     type: Sequelize.INTEGER,
+    //     allowNull: false,
+    //     onUpdate: 'CASCADE'
+    // },
     motivo_consulta: {
         type: Sequelize.STRING(68),
         allowNull: false,
@@ -31,10 +31,7 @@ const Appointment = connection.define('appointment', {
         notEmpty: true
     },
     hora_consulta: {
-        type: Sequelize.DATE,
-        get: function() {
-            return this.getDataValue('hora_consulta');
-        },
+        type: Sequelize.TIME,
         allowNull: false,
         notEmpty: true
     },
@@ -50,12 +47,13 @@ const Appointment = connection.define('appointment', {
     dosagem_precaucoes: {
         type: Sequelize.STRING(256),
         allowNull: false,
-        notEmpty: true
+        defaultValue: 'Medicação não receitada.'
     },
     status: {
         type: Sequelize.BOOLEAN,
         allowNull: false,
-        notEmpty: true
+        notEmpty: true,
+        defaultValue: false
     }
 });
 
