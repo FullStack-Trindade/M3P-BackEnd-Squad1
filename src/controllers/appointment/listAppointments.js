d76293ff3de293455f9ee003a98b28062faa3eeb
biachristie <beatrizchristie@yahoo.com.br>
2023-10-13 17:22:13 -0300

    added find all appointments to listAppointments

--- src/controllers/appointment/listAppointments.js
+++ src/controllers/appointment/listAppointments.js
@@ -1,6 +1,10 @@
+const Appointment = require('../../models/appointment');
+
 async function listAppointments (req, res) {
     try {
-        
+        const appointments = await Appointment.findAll();
+        return res.status(200).json(appointments);  
+
     } catch (error) {
         return res.status(500).json({ message: 'Requisição não pode ser executada' });
     }
