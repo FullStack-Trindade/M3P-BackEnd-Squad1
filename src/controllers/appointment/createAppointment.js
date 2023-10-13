ba41bc629e218577bd7ffbedc6c31167fb63f0c5
biachristie <beatrizchristie@yahoo.com.br>
2023-10-13 14:04:44 -0300

    translate error message in createAppointment

--- src/controllers/appointment/createAppointment.js
+++ src/controllers/appointment/createAppointment.js
@@ -1,23 +1,23 @@
-const Patient = require('../../models/patient');
-const Doctor = require('../../models/doctor');
+// const Patient = require('../../models/patient');
+// const Doctor = require('../../models/doctor');
 const Appointment = require('../../models/appointment');
 
 async function createAppointment (req, res) {
     try {
-        const patient = await Patient.findByPk(req.body.id_paciente);
-        const doctor = await Doctor.findByPk(req.body.id_medico);
+        // const patient = await Patient.findByPk(req.body.id_paciente);
+        // const doctor = await Doctor.findByPk(req.body.id_medico);
 
-        if (!patient) {
-            return res.status(404).json({ message: 'Patient not registered' });
-        }
+        // if (!patient) {
+        //     return res.status(404).json({ message: 'Patient not registered' });
+        // }
 
-        if (!doctor) {
-            return res.status(404).json({ message: 'Doctor not registered' });
-        }
+        // if (!doctor) {
+        //     return res.status(404).json({ message: 'Doctor not registered' });
+        // }
 
         const appointment = {
-            id_paciente: patient.id,
-            id_medico: doctor.id,
+            // id_paciente: patient.id,
+            // id_medico: doctor.id,
             motivo_consulta: req.body.motivo_consulta,
             data_consulta: req.body.data_consulta,
             hora_consulta: req.body.hora_consulta,
@@ -34,7 +34,7 @@ async function createAppointment (req, res) {
         });
     
     } catch (error) {
-        return res.status(500).json({ message: 'Cannot perform requested requisition' });
+        return res.status(500).json({ message: 'Requisição não pode ser executada' });
     }
 }
 
