59223f81e2248e5ed03b5424ff66b6ba32166501
biachristie <beatrizchristie@yahoo.com.br>
2023-10-14 17:23:51 -0300

    added error status to deleteAppointmentById

--- src/controllers/appointment/deleteAppointmentById.js
+++ src/controllers/appointment/deleteAppointmentById.js
@@ -1,5 +1,11 @@
-async function deleteAppointmentById (req, res) {
+const Appointment = require("../../models/appointment");
 
+async function deleteAppointmentById (req, res) {
+    try {
+
+    } catch (error) {
+        return res.status(500).json({ message: 'Requisição não pode ser executada' });
+    }
 }
 
 module.exports = deleteAppointmentById;
