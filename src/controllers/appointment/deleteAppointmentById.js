43e68b1f62f92e4c1c684201c6ab853b882f2b2c
biachristie <beatrizchristie@yahoo.com.br>
2023-10-14 17:47:28 -0300

    added validations to deleteAppointmentById

--- src/controllers/appointment/deleteAppointmentById.js
+++ src/controllers/appointment/deleteAppointmentById.js
@@ -2,7 +2,17 @@ const Appointment = require("../../models/appointment");
 
 async function deleteAppointmentById (req, res) {
     try {
-        const appointment = await Appointment.findByPk(req.params.id);
+        const id = req.params.id;
+        
+        if (id <= 0) {
+            return res.status(400).json({ message: 'Identificador deve ser um número positivo' });
+        }
+
+        const appointment = await Appointment.findByPk(id);
+
+        if (!appointment) {
+            return res.status(400).json({ message: 'Consulta não encontrada' });
+        }
 
         await appointment.destroy();
         return res.status(202).json();
