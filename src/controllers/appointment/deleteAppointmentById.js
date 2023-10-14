3d12dd0310546c61fbdc9d4c84e5f07f9fa9045b
biachristie <beatrizchristie@yahoo.com.br>
2023-10-14 17:24:32 -0300

    added appointment destroy to deleteAppointmentById

--- src/controllers/appointment/deleteAppointmentById.js
+++ src/controllers/appointment/deleteAppointmentById.js
@@ -2,6 +2,10 @@ const Appointment = require("../../models/appointment");
 
 async function deleteAppointmentById (req, res) {
     try {
+        const appointment = await Appointment.findByPk(req.params.id);
+
+        await appointment.destroy();
+        return res.status(202).json();
 
     } catch (error) {
         return res.status(500).json({ message: 'Requisição não pode ser executada' });
