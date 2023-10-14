5e2d2d0fb97a2dfc20b0f5d582317eb426822d4c
biachristie <beatrizchristie@yahoo.com.br>
2023-10-14 17:49:30 -0300

    added delete appointments by id route

---
+++ src/routes/appointment.js
@@ -0,0 +1,19 @@
+const { Router } = require('express');
+
+// MIDDLEWARES
+const register = require('../middlewares/appointment/validate-create-appointment');
+
+// CONTROLLERS
+const createAppointment = require('../controllers/appointment/createAppointment');
+const listAppointment = require('../controllers/appointment/listAppointment');
+const deleteAppointmentById = require('../controllers/appointment/deleteAppointmentById');
+
+// ROUTES
+const appointmentRoutes = new Router();
+
+appointmentRoutes
+    .post('/api/consultas', register.validateCreateAppointment, createAppointment)
+    .get('/api/consultas', listAppointment)
+    .delete('/api/consultas/:id', deleteAppointmentById);
+
+module.exports = appointmentRoutes;
