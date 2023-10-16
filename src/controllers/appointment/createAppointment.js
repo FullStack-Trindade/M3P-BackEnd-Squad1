649ce33f49d7176c0ade731693e8d262ddf14446
biachristie <beatrizchristie@yahoo.com.br>
2023-10-16 20:04:09 -0300

    added id patient and id doctor to createAppointment

--- src/controllers/appointment/createAppointment.js
+++ src/controllers/appointment/createAppointment.js
@@ -1,37 +1,33 @@
-// const Patient = require('../../models/patient');
-// const Doctor = require('../../models/doctor');
+const User = require('../../models/user');
 const Appointment = require('../../models/appointment');
 
 async function createAppointment (req, res) {
     try {
-        // const patient = await Patient.findByPk(req.body.id_paciente);
-        // const doctor = await Doctor.findByPk(req.body.id_medico);
+        const patient = await User.findByPk(req.body.id_patient);
+        const doctor = await User.findByPk(req.body.id_doctor);
 
-        // if (!patient) {
-        //     return res.status(404).json({ message: 'Patient not registered' });
-        // }
+        if (!patient || patient.id_type !== 1) {
+            return res.status(404).json({ message: 'Paciente não consta nos registros' });
+        }
 
-        // if (!doctor) {
-        //     return res.status(404).json({ message: 'Doctor not registered' });
-        // }
+        if (!doctor || doctor.id_type !== 3) {
+            return res.status(404).json({ message: 'Médico não consta nos registros' });
+        }
 
         const appointment = {
-            // id_paciente: patient.id,
-            // id_medico: doctor.id,
-            motivo_consulta: req.body.motivo_consulta,
-            data_consulta: req.body.data_consulta,
-            hora_consulta: req.body.hora_consulta,
-            descricao_problema: req.body.descricao_problema,
-            medicacao_receitada: req.body.medicacao_receitada,
-            dosagem_precaucoes: req.body.dosagem_precaucoes,
+            id_patient: patient.id,
+            id_doctor: doctor.id,
+            appointment_reason: req.body.appointment_reason,
+            appointment_date: req.body.appointment_date,
+            appointment_hour: req.body.appointment_hour,
+            problem_description: req.body.problem_description,
+            medication_prescribed: req.body.medication_prescribed,
+            dosage_precautions: req.body.dosage_precautions,
             status: req.body.status
         };
 
         const newAppointment = await Appointment.create(appointment);
-        return res.status(201).json({
-            id: newAppointment.id,
-            appointment
-        });
+        return res.status(201).json(newAppointment);
     
     } catch (error) {
         return res.status(500).json({ message: 'Requisição não pode ser executada' });
