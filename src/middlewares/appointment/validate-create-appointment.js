923effb80d5243397c83b17cb5f6bb74735766e1
biachristie <beatrizchristie@yahoo.com.br>
2023-10-16 20:06:57 -0300

    added id patient and id doctor to validate-create-appointment

--- src/middlewares/appointment/validate-create-appointment.js
+++ src/middlewares/appointment/validate-create-appointment.js
@@ -1,35 +1,33 @@
 const yup = require('yup');
 
 const validation = yup.object().shape({
-    // id_paciente: yup
-    //     .number()
-    //     .required('ID do paciente é obrigatório.'),
-    // id_medico: yup
-    //     .number()
-    //     .required('ID do médico é obrigatório.'),
-    motivo_consulta: yup
+    id_patient: yup
+        .number()
+        .required('ID do paciente é obrigatório.'),
+    id_doctor: yup
+        .number()
+        .required('ID do médico é obrigatório.'),
+    appointment_reason: yup
         .string()
         .min(8, 'Motivo da consulta deve ter, no mínimo, 8 caracteres')
         .max(64, 'Motivo da consulta deve ter, no máxnimo, 64 caracteres')
         .required('Motivo da consulta é obrigatória'),
-    data_consulta: yup
+    appointment_date: yup
         .date()
-        // .typeError('Data da consulta deve estar em formato YYYY-MM-DD')
-        // #TODO: ajustar formato de data; atualmente aceita datas no formato YYYY-MM-DD e MM-DD-YYYY e mensagem não aparece
         .min(new Date().toISOString().slice(0,10), 'Não é possível incluir datas anteriores ao dia atual')
         .required('Data da consulta é obrigatória'),
-    hora_consulta: yup
+    appointment_hour: yup
         .string()
         .required('Hora da consulta é obrigatória'),
-    descricao_problema: yup
+    problem_description: yup
         .string()
         .min(16, 'Descrição do problema teve ter, no mínimo, 16 caracteres')
         .max(1024, 'Descrição do problema deve ter, no máximo, 1024 caracteres')
         .required('Descrição do problema é obrigatório'),
-    medicacao_receitada: yup
+    medication_prescribed: yup
         .string()
         .notRequired(),
-    dosagem_precaucoes: yup
+    dosage_precautions: yup
         .string()
         .min(16, 'Dosagem e precauções teve ter, no mínimo, 16 caracteres')
         .max(256, 'Dosagem e precauções deve ter, no máximo, 256 caracteres')
