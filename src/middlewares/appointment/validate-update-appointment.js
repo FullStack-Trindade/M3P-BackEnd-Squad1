e8a852cca88284835b4e145eeda9fc044017b11c
biachristie <beatrizchristie@yahoo.com.br>
2023-10-17 18:01:41 -0300

    validate update appointment attributes translated to en

--- src/middlewares/appointment/validate-update-appointment.js
+++ src/middlewares/appointment/validate-update-appointment.js
@@ -1,29 +1,27 @@
 const yup = require('yup');
 
 const validation = yup.object().shape({
-    motivo_consulta: yup
+    appointment_reason: yup
         .string()
         .min(8, 'Motivo da consulta deve ter, no mínimo, 8 caracteres')
         .max(64, 'Motivo da consulta deve ter, no máximo, 64 caracteres')
         .required('Motivo da consulta é obrigatória'),
-    data_consulta: yup
+    appointment_date: yup
         .date()
-        // .typeError('Data da consulta deve estar em formato YYYY-MM-DD')
-        // #TODO: ajustar formato de data; atualmente aceita datas no formato YYYY-MM-DD e MM-DD-YYYY e mensagem não aparece
         .min(new Date().toISOString().slice(0,10), 'Não é possível incluir datas anteriores ao dia atual')
         .required('Data da consulta é obrigatória'),
-    hora_consulta: yup
+    appointment_hour: yup
         .string()
         .required('Hora da consulta é obrigatória'),
-    descricao_problema: yup
+    problem_description: yup
         .string()
         .min(16, 'Descrição do problema teve ter, no mínimo, 16 caracteres')
         .max(1024, 'Descrição do problema deve ter, no máximo, 1024 caracteres')
         .required('Descrição do problema é obrigatório'),
-    medicacao_receitada: yup
+    medication_prescribed: yup
         .string()
         .notRequired(),
-    dosagem_precaucoes: yup
+    dosage_precautions: yup
         .string()
         .min(16, 'Dosagem e precauções teve ter, no mínimo, 16 caracteres')
         .max(256, 'Dosagem e precauções deve ter, no máximo, 256 caracteres')
