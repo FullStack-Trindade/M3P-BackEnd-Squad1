478017c01c980d4dd2352fc3ccf4a57099b163fc
biachristie <beatrizchristie@yahoo.com.br>
2023-10-17 18:06:21 -0300

    added validations to listAppointments

--- src/controllers/appointment/listAppointments.js
+++ src/controllers/appointment/listAppointments.js
@@ -2,23 +2,26 @@ const Appointment = require('../../models/appointment');
 
 async function listAppointments (req, res) {
     try {
-        // const filter = req.query;
+        const filter = req.query;
 
-        // if (filter.id) {
-        //     if (typeof Number(filter.id) !== 'number') {
-        //         return res.status(400).json({ message: 'O ID do paciente deve ser um número' })
-        //     }
+        if (filter.id) {
+            if (filter.id <= 0) {
+                return res.status(400).json({ message: 'O ID do paciente deve ser um número positivo' });
+            }
 
-        //     #TODO: verificar se a requisição funciona com id_paciente
-        //     const appointments = await Appointment.findAll(
-        //         { where: { id_paciente: filter.id } }
-        //     );
+            const appointments = await Appointment.findAll(
+                { where: { id_patient: filter.id } }
+                );
 
-        //     return res.status(200).json(appointments);
-        // } else {
-            const appointments = await Appointment.findAll();
+            if (appointments.length === 0) {
+                return res.status(400).json({ message: 'Paciente não possui consultas cadastradas' });
+            }
+
             return res.status(200).json(appointments);
-        // }  
+        }
+        
+        const appointments = await Appointment.findAll();
+        return res.status(200).json(appointments);
 
     } catch (error) {
         return res.status(500).json({ message: 'Requisição não pode ser executada' });
