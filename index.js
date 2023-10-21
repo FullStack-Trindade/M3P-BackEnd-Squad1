940c68863f92509609ab312bc698cbc304fd0eee
rodrigopieritz <rodrigopieritz@gmail.com>
2023-10-21 12:57:48 -0300

    comentado código duplicado

--- index.js
+++ index.js
@@ -36,9 +36,10 @@ const Login = require('./src/controllers/session/login')
 const validateToken = require('./src/middlewares/validateToken')
 app.post('/api/usuario/login', validateToken, Login)
 
-const postUser = require('./src/controllers/user/postUser')
-const validaUsuario = require('./src/middlewares/validaUsuario')
-app.post('/api/usuario', validaUsuario, postUser)
+// estava duplicado
+// const postUser = require('./src/controllers/user/postUser')
+// const validaUsuario = require('./src/middlewares/validaUsuario')
+// app.post('/api/usuario', validaUsuario, postUser)
 
 app.listen(process.env.SERVER_PORT, () => {
   console.log("local server online");
