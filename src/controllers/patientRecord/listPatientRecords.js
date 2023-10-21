636ea91ea3ab5f556ee6b85b50128eef543bbe54
biachristie <beatrizchristie@yahoo.com.br>
2023-10-21 19:48:02 -0300

    added find all patients to listPatientRecords

--- src/controllers/patientRecord/listPatientRecords.js
+++ src/controllers/patientRecord/listPatientRecords.js
@@ -1,10 +1,23 @@
+const Patient = require('../../models/patient');
+
 async function listPatientRecords (req, res) {
     try {
+        const patients = await Patient.findAll({ include: { all: true } });
+
+        const filteredPatients = [];
+        patients.forEach(patient => {
+            if (patient.appointments.length > 0
+                // || patient.exams.length > 0
+                ) {
+                filteredPatients.push(patient);
+            }
+        })
+
+        return res.status(200).json(filteredPatients);
 
     } catch (error) {
         return res.status(500).json({ message: 'Requisição não pode ser executada' });
     }
-
 }
 
 module.exports = listPatientRecords;
