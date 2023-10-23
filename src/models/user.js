e81bef2bbc1509689a5fac2d9c8a90e7888f2d55
rodrigopieritz <rodrigopieritz@gmail.com>
2023-10-23 19:46:35 -0300

    incluida linhas para atualização do fuso no db

--- src/models/user.js
+++ src/models/user.js
@@ -45,6 +45,17 @@ const User = connection.define('user', {
     //     type: Sequelize.STRING,
     //     allowNull: false
     // },
+
+    created_at: {
+        type: 'TIMESTAMP',
+        allowNull: false,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+    },
+    updated_at: {
+        type: 'TIMESTAMP',
+        allowNull: false,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+    }
 });
 
 User.belongsTo(Type, { foreignKey: 'id_type' });
