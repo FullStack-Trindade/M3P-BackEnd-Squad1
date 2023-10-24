fa8e665c860cde238be3a09e2c319f5cb1dbe7dc
biachristie <beatrizchristie@yahoo.com.br>
2023-10-24 15:46:03 -0300

    updated address model

--- src/models/adress.js
+++ src/models/adress.js
@@ -1,3 +1,21 @@
+// const connection = require("../database/index");
+// const { Sequelize } = require("sequelize");
+
+// const Adress = connection.define("adress", {
+//   id: {
+//     type: Sequelize.INTEGER,
+//     autoIncrement: true,
+//     primaryKey: true,
+//   },
+
+//   adress: {
+//     type: Sequelize.JSONB,
+//     allowNull: false,
+//   },
+// });
+
+// module.exports = Adress;
+
 const connection = require("../database/index");
 const { Sequelize } = require("sequelize");
 
@@ -7,11 +25,46 @@ const Adress = connection.define("adress", {
     autoIncrement: true,
     primaryKey: true,
   },
-
-  adress: {
-    type: Sequelize.JSONB,
+  cep: {
+    type: Sequelize.STRING, // Tipo de dado corrigido para string
     allowNull: false,
   },
+  city: {
+    type: Sequelize.STRING, // Tipo de dado corrigido para string
+    allowNull: false,
+  },
+  state: {
+    type: Sequelize.STRING, // Tipo de dado corrigido para string
+    allowNull: false,
+  },
+  street: {
+    type: Sequelize.STRING, // Tipo de dado corrigido para string
+    allowNull: false,
+  },
+  number: {
+    type: Sequelize.STRING, // Tipo de dado corrigido para string
+    allowNull: false,
+  },
+  complement: {
+    type: Sequelize.STRING, // Tipo de dado corrigido para string
+  },
+  neighborhood: {
+    type: Sequelize.STRING, // Tipo de dado corrigido para string
+    allowNull: false,
+  },
+  reference: {
+    type: Sequelize.STRING, // Tipo de dado corrigido para string
+  },
+  created_at: {
+    type: 'TIMESTAMP',
+    allowNull: false,
+    defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+},
+updated_at: {
+    type: 'TIMESTAMP',
+    allowNull: false,
+    defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+}
 });
 
 module.exports = Adress;
