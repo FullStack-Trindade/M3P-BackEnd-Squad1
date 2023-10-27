b7e80a61a67643e1fe683b7d71402950f5029cc7
gabrieldelpacheco <gabriel.del.pacheco@gmail.com>
2023-10-27 18:55:00 -0300

    criado validate do exame update  e mudado para ingles o validade exame.

--- src/middlewares/validate-examsUpdate.js
+++ src/middlewares/validate-examsUpdate.js
@@ -1,40 +1,40 @@
 const yup = require("yup");
 
 const validation = yup.object().shape({
-    id_medico: yup
+    id_doctor: yup
     .number()
     .typeError("ID do médico deve ser um número.")
     .required("ID do médico é obrigatório.")
     .positive("ID do médico deve ser positivo.")
     .integer("ID do médico deve ser um número inteiro."),
-    nomeExame: yup
+    nameExam: yup
     .string()
     .max(64, "O campo tipo deve ter no máximo 64 caracteres")
     .min(8, "O campo nome do exame tem no mínimo 8 caracteres")
     .required("O campo nome do exame é obrigatório"),
-    dataExame: yup
+    dateExam: yup
     .string()
     .required("O campo data é obrigatório"),
-    horaExame: yup
+    hourExam: yup
     .string()
     .required("O campo hora é obrigatório"),
-    tipoExame: yup
+    typeExam: yup
     .string()
     .max(32, "O campo tipo deve ter no máximo 64 caracteres")
     .min(4, "O campo nome do exame tem no mínimo 8 caracteres")
     .required("O campo tipo do exame é obrigatório"),
-    labExame: yup
+    labExam: yup
     .string()
     .max(32, "O campo nome do laboratório é obrigatório ter no máximo 32 caracteres.")
     .min(4,"O campo nome do laboratório tem no mínimo 8 caracteres"),
-    resultaDoExame: yup
+    resultExam: yup
     .string()
     .max(1024, "O campo resultado deve ter no máximo 64 caracteres")
     .min(16, "O campo resultado do exame tem no mínimo 16 caracteres",)
     .required("O campo resultado do exame é obrigatório"),
 });
 
-function validacaoExame(request, response, next) {
+function validateExam(request, response, next) {
   try {
     validation.validateSync(request.body);
     next();
@@ -43,4 +43,4 @@ function validacaoExame(request, response, next) {
   }
 }
 
-module.exports = validacaoExame;
+module.exports = validateExam;
