44d0ecb9d199b063f625c1de4cfb9cdd7a7cf18f
biachristie <beatrizchristie@yahoo.com.br>
2023-10-27 09:45:50 -0300

    bugfix: deleted duplicated row at validateToken

--- src/middlewares/validateToken.js
+++ src/middlewares/validateToken.js
@@ -12,8 +12,7 @@ const validateToken = (request, response, next)=>{
 
 
     const newToken = token.slice(7)
-    let newToken = token.slice(8)
-    newToken = newToken.substring(0, newToken.length - 1) */
+    newToken = newToken.substring(0, newToken.length - 1)
 
 
     jwt.verify(newToken,'secret_key', (error, conteudoDoToken)=>{
