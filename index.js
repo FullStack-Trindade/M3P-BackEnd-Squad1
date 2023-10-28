0c233c1aa2197ea0c00c4b881eba6519130f8fb7
rodrigopieritz <rodrigopieritz@gmail.com>
2023-10-28 19:02:23 -0300

    necessário para front

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-require('dotenv').config()
-const express = require('express')
-const cors = require('cors');
-const appointmentRoutes = require('./src/routes/appointment')
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const appointmentRoutes = require("./src/routes/appointment");
 
-const connection = require('./src/database/index');
+const connection = require("./src/database/index");
 
 //Autenticação
 const Login = require("./src/controllers/session/login");
@@ -15,18 +15,18 @@ const updatePatient = require("./src/controllers/Patients/updatePatients");
 const patientList = require("./src/controllers/Patients/patientList");
 const searchPatients = require("./src/controllers/Patients/searchPatients");
 const deletePatient = require("./src/controllers/Patients/deletePatients");
+const searchPatientByIdUser = require("./src/controllers/Patients/searchPatientByIdUser");
 
 //Usuário
 const postUser = require("./src/controllers/user/postUser");
 const putUser = require("./src/controllers/user/putUser");
-const getUser = require('./src/controllers/user/getUser');
-const searchUserByCpf = require('./src/controllers/user/searchUserByCpfEmail');
+const getUser = require("./src/controllers/user/getUser");
+const searchUserByCpf = require("./src/controllers/user/searchUserByCpfEmail");
 
 //Midleware
 const validaUsuario = require("./src/middlewares/validaUsuario");
 const validatePatientRequest = require("./src/middlewares/validate-patient-request");
 const validatePutUser = require("./src/middlewares/validatePutUser");
-
 
 const app = express();
 app.use(express.json());
@@ -43,35 +43,32 @@ app.put("/api/pacientes/:id", validatePatientRequest, updatePatient);
 app.get("/api/pacientes", patientList);
 app.get("/api/pacientes/:id", searchPatients);
 app.delete("/api/pacientes/:id", deletePatient);
+app.get("/api/pacientes/usuario/:id", searchPatientByIdUser);
 
 //Usuário
 app.post("/api/usuario", validaUsuario, postUser);
-app.put("/api/usuarios/:id",validatePutUser,putUser );
-app.get("/api/usuarios",getUser);
-app.post("/api/usuarios/search",searchUserByCpf);
-
+app.put("/api/usuarios/:id", validatePutUser, putUser);
+app.get("/api/usuarios", getUser);
+app.post("/api/usuarios/search", searchUserByCpf);
 
 //Consultas
 app.use(appointmentRoutes);
 
 const startServer = () => {
-
   app.listen(process.env.SERVER_PORT, () => {
     console.log(`Servidor rodando na porta ${process.env.SERVER_PORT}`);
   });
-  };
+};
 
-
-const connect = async() => {
+const connect = async () => {
   try {
-    await connection.authenticate()
-    console.log('Conexão com banco de dados bem sucedida');
+    await connection.authenticate();
+    console.log("Conexão com banco de dados bem sucedida");
     startServer();
   } catch (error) {
-    console.log('Sem conexao com banco de dados', error);
+    console.log("Sem conexao com banco de dados", error);
   }
-}
+};
 
-connect()
+connect();
 connection.sync({ alter: true });
-
