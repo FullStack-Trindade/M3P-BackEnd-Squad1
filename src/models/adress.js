adbd8bd8844440e1e033af7d684f664250a2517c
biachristie <beatrizchristie@yahoo.com.br>
2023-10-29 19:45:02 -0300

    added fk association to address model

--- src/models/adress.js
+++ src/models/adress.js
@@ -19,6 +19,8 @@
 const connection = require("../database/index");
 const { Sequelize } = require("sequelize");
 
+const Patient = require('./patient');
+
 const Adress = connection.define("adress", {
   id: {
     type: Sequelize.INTEGER,
@@ -26,45 +28,48 @@ const Adress = connection.define("adress", {
     primaryKey: true,
   },
   cep: {
-    type: Sequelize.STRING, // Tipo de dado corrigido para string
+    type: Sequelize.STRING, 
     allowNull: false,
   },
   city: {
-    type: Sequelize.STRING, // Tipo de dado corrigido para string
+    type: Sequelize.STRING, 
     allowNull: false,
   },
   state: {
-    type: Sequelize.STRING, // Tipo de dado corrigido para string
+    type: Sequelize.STRING, 
     allowNull: false,
   },
   street: {
-    type: Sequelize.STRING, // Tipo de dado corrigido para string
+    type: Sequelize.STRING, 
     allowNull: false,
   },
   number: {
-    type: Sequelize.STRING, // Tipo de dado corrigido para string
+    type: Sequelize.STRING, 
     allowNull: false,
   },
   complement: {
-    type: Sequelize.STRING, // Tipo de dado corrigido para string
+    type: Sequelize.STRING,
   },
   neighborhood: {
-    type: Sequelize.STRING, // Tipo de dado corrigido para string
+    type: Sequelize.STRING,
     allowNull: false,
   },
   reference: {
-    type: Sequelize.STRING, // Tipo de dado corrigido para string
-  },
+    type: Sequelize.STRING, 
+    },
   created_at: {
     type: 'TIMESTAMP',
     allowNull: false,
     defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
-},
-updated_at: {
+  },
+  updated_at: {
     type: 'TIMESTAMP',
     allowNull: false,
     defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
-}
+  }
 });
+
+Adress.hasOne(Patient, { sourceKey: 'id', foreignKey: 'adressId' });
+Patient.belongsTo(Adress, { foreignKey: 'adressId' });
 
 module.exports = Adress;
