f003af2b8858ca3118dba8c74b9391151afed558
biachristie <beatrizchristie@yahoo.com.br>
2023-10-29 12:06:57 -0300

    changed id_type to id and added created_at and updated_at to type model

--- src/models/type.js
+++ src/models/type.js
@@ -3,13 +3,23 @@ const connection = require('../config/database');
 const sequelize = new Sequelize(connection);
 
 const Type = sequelize.define('types', {
-  id_type: {
+  id: {
     type: Sequelize.INTEGER,
     primaryKey: true
   },
   descricao: {
     type: Sequelize.STRING,
     allowNull: false
+  },
+  created_at: {
+    type: 'TIMESTAMP',
+    allowNull: false,
+    defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+  },
+  updated_at: {
+    type: 'TIMESTAMP',
+    allowNull: false,
+    defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
   }
 });
 
