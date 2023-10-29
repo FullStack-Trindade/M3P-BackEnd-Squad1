eef76df856f5d6d170bc1955c275cc38b0f0726f
biachristie <beatrizchristie@yahoo.com.br>
2023-10-29 12:39:30 -0300

    added create appointments to migrations

---
+++ src/migrations/20231029140614-create-appointments.js
@@ -0,0 +1,80 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('appointments', {
+      id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        unique: true,
+        primaryKey: true,
+        onUpdate: 'CASCADE'
+      },
+      id_patient: {
+          type: Sequelize.INTEGER,
+          allowNull: false,
+          onUpdate: 'CASCADE',
+          references: {
+            model: { tableName: 'patients', key: 'id' }
+          }
+      },
+      id_doctor: {
+          type: Sequelize.INTEGER,
+          allowNull: false,
+          onUpdate: 'CASCADE',
+          references: {
+            model: { tableName: 'users', key: 'id' }
+          }
+      },
+      appointment_reason: {
+          type: Sequelize.STRING(64),
+          allowNull: false,
+          notEmpty: true
+      },
+      appointment_date: {
+          type: Sequelize.DATEONLY,
+          allowNull: false,
+          notEmpty: true
+      },
+      appointment_hour: {
+          type: Sequelize.TIME,
+          allowNull: false,
+          notEmpty: true
+      },
+      problem_description: {
+          type: Sequelize.STRING(1024),
+          allowNull: false,
+          notEmpty: true
+      },
+      medication_prescribed: {
+          type: Sequelize.STRING(1024),
+          allowNull: true
+      },
+      dosage_precautions: {
+          type: Sequelize.STRING(256),
+          allowNull: false,
+          defaultValue: 'Medicação não receitada.'
+      },
+      status: {
+          type: Sequelize.BOOLEAN,
+          allowNull: false,
+          notEmpty: true,
+          defaultValue: true
+      },
+      created_at: {
+          type: 'TIMESTAMP',
+          allowNull: false,
+          defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+      },
+      updated_at: {
+          type: 'TIMESTAMP',
+          allowNull: false,
+          defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('appointments');
+  }
+};
