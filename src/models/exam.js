8c7ed20d4ca319cb72e499e1c1e152136debde22
biachristie <beatrizchristie@yahoo.com.br>
2023-10-29 12:32:42 -0300

    modified sequelize connection at exam model

--- src/models/exam.js
+++ src/models/exam.js
@@ -1,16 +1,15 @@
-const connection = require('../database/index');
-const { Sequelize} = require('sequelize');
+const { Sequelize } = require('sequelize');
+const connection = require('../config/database');
+const sequelize = new Sequelize(connection);
 
 const Patient = require('./patient')
 
-const Exam = connection.define('exam', {
-
+const Exam = sequelize.define('exams', {
     id: {
         type: Sequelize.INTEGER,
         autoIncrement: true,
         primaryKey: true,
     },
-
     id_patient:{
         type: Sequelize.INTEGER,
         allowNull: false,
@@ -19,42 +18,34 @@ const Exam = connection.define('exam', {
         type: Sequelize.INTEGER,
         allowNull: false,
     },
-
     nameExam: {
         type: Sequelize.STRING,
         allowNull: false,
     },
-
     dateExam: {
         type: Sequelize.DATEONLY,
         allowNull: false,
     },
-
     hourExam: {
         type: Sequelize.STRING,
         allowNull: false,
     },
-
     typeExam: {
         type: Sequelize.STRING,
         allowNull: false,
     },
-
     labExam: {
         type: Sequelize.STRING,
         allowNull: false,
     },
-
     urlExam: {
         type: Sequelize.STRING,
         allowNull: true,
     },
-
     resultExam:{
         type: Sequelize.STRING,
         allowNull: false,
     },
-
     statusExam: {
         type: Sequelize.BOOLEAN,
         allowNull: false,
@@ -63,6 +54,6 @@ const Exam = connection.define('exam', {
 
 });
 
-    Exam.belongsTo(Patient, {foreignKey:"id_patient"});
+Exam.belongsTo(Patient, {foreignKey:"id_patient"});
 
 module.exports = Exam;
