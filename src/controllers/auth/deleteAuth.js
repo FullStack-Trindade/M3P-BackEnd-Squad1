d843631c3d73165f85fecc2acca728ceb24612d3
biachristie <beatrizchristie@yahoo.com.br>
2023-10-31 12:11:45 -0300

    added destroy to deleteAuth

--- src/controllers/auth/deleteAuth.js
+++ src/controllers/auth/deleteAuth.js
@@ -1,5 +1,13 @@
+const Auth = require("../../models/auth");
+
 async function deleteAuth (req, res) {
     try {
+        const id = req.params.id;
+
+        const authData = await Auth.findByPk(id);
+
+        await authData.destroy();
+        return res.status(202).json();
 
     } catch (error) {
         return res.status(500).json({ message: 'Requisição não pode ser executada' });
