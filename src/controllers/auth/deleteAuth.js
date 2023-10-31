ca8baa7380905ee3502885b14b431393453265b2
biachristie <beatrizchristie@yahoo.com.br>
2023-10-31 12:12:18 -0300

    added validations to deleteAuth

--- src/controllers/auth/deleteAuth.js
+++ src/controllers/auth/deleteAuth.js
@@ -4,7 +4,15 @@ async function deleteAuth (req, res) {
     try {
         const id = req.params.id;
 
+        if (id <= 0) {
+            return res.status(400).json({ message: 'Identificador deve ser um número positivo' });
+        }
+
         const authData = await Auth.findByPk(id);
+
+        if (!authData) {
+            return res.status(400).json({ message: 'Autorização não encontrada' });
+        }
 
         await authData.destroy();
         return res.status(202).json();
