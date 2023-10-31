8de12c2bf9b5ac445635f88e185ed7234fa3af82
biachristie <beatrizchristie@yahoo.com.br>
2023-10-31 16:49:45 -0300

    added updateDiet and validateUpdateDiet to diet routes

--- src/routes/diet.js
+++ src/routes/diet.js
@@ -2,14 +2,17 @@ const { Router } = require('express');
 
 // MIDDLEWARES
 const register = require('../middlewares/diet/validate-create-diet');
+const update = require('../middlewares/diet/validate-update-diet');
 
 // CONTROLLERS
 const createDiet = require('../controllers/diet/createDiet');
+const updateDiet = require('../controllers/diet/updateDiet');
 
 // ROUTES
 const dietRoutes = new Router();
 
 dietRoutes
     .post('/api/dietas', register.validateCreateDiet, createDiet)
+    .put('/api/dietas/:id', update.validateCreateDiet, updateDiet)
 
 module.exports = dietRoutes;
