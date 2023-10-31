f274a4ccbc7209e1de5f51f0e69d992318b5a84a
biachristie <beatrizchristie@yahoo.com.br>
2023-10-31 17:21:21 -0300

    added error to deletaDietById

--- src/controllers/diet/deleteDietById.js
+++ src/controllers/diet/deleteDietById.js
@@ -1,5 +1,9 @@
 async function deleteDietById (req, res) {
-
+    try {
+        
+    } catch (error) {
+        return res.status(500).json({ message: 'Requisição não pode ser executada' });
+    }
 }
 
 module.exports = deleteDietById;
