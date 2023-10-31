ed3d1f2e6d76c2e812ac339c39f3f096c0d57acb
biachristie <beatrizchristie@yahoo.com.br>
2023-10-31 17:01:40 -0300

    added error to listDiets

--- src/controllers/diet/listDiets.js
+++ src/controllers/diet/listDiets.js
@@ -1,5 +1,9 @@
 async function listDiets (req, res) {
-    
+    try {
+
+    } catch (error) {
+        return res.status(500).json({ message: 'Requisição não pode ser executada' });
+    }
 }
 
 module.exports = listDiets;
