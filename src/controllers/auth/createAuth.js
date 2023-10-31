73deca96b393a02fbf350241c9718b24f42dc38d
biachristie <beatrizchristie@yahoo.com.br>
2023-10-31 12:08:00 -0300

    added newAuth to createAuth

--- src/controllers/auth/createAuth.js
+++ src/controllers/auth/createAuth.js
@@ -1,5 +1,17 @@
+const Auth = require("../../models/auth");
+
 async function createAuth (req, res) {
     try {
+        const { id_user, token_user, id_type } = req.body;
+
+        const authData = {
+            id_user: id_user,
+            token_user: token_user,
+            id_type: id_type
+        };
+
+        const newAuth = await Auth.create(authData);
+        return res.status(201).json(newAuth);
     
     } catch (error) {
         return res.status(500).json({ message: 'Requisição não pode ser executada' });
