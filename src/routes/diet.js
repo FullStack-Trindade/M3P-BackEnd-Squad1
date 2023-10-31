21a82dc13872e33b8dd16507cb3a2b04f4618d12
biachristie <beatrizchristie@yahoo.com.br>
2023-10-31 17:05:23 -0300

    added listDiets to diet routes

--- src/routes/diet.js
+++ src/routes/diet.js
@@ -5,11 +5,16 @@ const register = require('../middlewares/diet/validate-create-diet');
 
 // CONTROLLERS
 const createDiet = require('../controllers/diet/createDiet');
+const updateDiet = require('../controllers/diet/updateDiet');
+const listDiets = require('../controllers/diet/listDiets');
 
 // ROUTES
 const dietRoutes = new Router();
 
 dietRoutes
     .post('/api/dietas', register.validateCreateDiet, createDiet)
+    .put('/api/dietas/:id', update.validateCreateDiet, updateDiet)
+    .get('/api/dietas', listDiets)
+
 
 module.exports = dietRoutes;
