296fff449a3b0672db69a8b27faab6853198e880
biachristie <beatrizchristie@yahoo.com.br>
2023-10-31 17:22:27 -0300

    added destroy to deleteDietById

--- src/controllers/diet/deleteDietById.js
+++ src/controllers/diet/deleteDietById.js
@@ -1,6 +1,14 @@
+const Diet = require("../../models/diet");
+
 async function deleteDietById (req, res) {
     try {
-        
+        const id = req.params.id;
+
+        const diet = await Diet.findByPk(id);
+
+        await diet.destroy();
+        return res.status(202).json();
+
     } catch (error) {
         return res.status(500).json({ message: 'Requisição não pode ser executada' });
     }
