abbcd55dfc464e8f6041bd149e458756368862ad
biachristie <beatrizchristie@yahoo.com.br>
2023-10-31 12:09:29 -0300

    added validation to createAuth

--- src/controllers/auth/createAuth.js
+++ src/controllers/auth/createAuth.js
@@ -4,6 +4,10 @@ async function createAuth (req, res) {
     try {
         const { id_user, token_user, id_type } = req.body;
 
+        if (!id_user || !token_user || !(id_type >= 0 && id_type <= 3) ) {
+            return res.status(400).json({ message: 'Dados incompletos. Por favor, reenvie os dados.' });
+        }
+
         const authData = {
             id_user: id_user,
             token_user: token_user,
