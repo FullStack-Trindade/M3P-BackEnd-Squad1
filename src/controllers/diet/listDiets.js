afbff41b92d6cd762a9e016e5843066f57f9ea94
biachristie <beatrizchristie@yahoo.com.br>
2023-10-31 17:02:50 -0300

    added findAll to listDiets

--- src/controllers/diet/listDiets.js
+++ src/controllers/diet/listDiets.js
@@ -1,5 +1,10 @@
+const Diet = require('../../models/diet');
+
 async function listDiets (req, res) {
     try {
+        
+        const diets = await Diet.findAll();
+        return res.status(200).json(diets);
 
     } catch (error) {
         return res.status(500).json({ message: 'Requisição não pode ser executada' });
