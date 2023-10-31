7d4f1e8873095ac4032944b5ad5a7fdabdecfb22
biachristie <beatrizchristie@yahoo.com.br>
2023-10-31 17:04:01 -0300

    added find by id filter to listDiets

--- src/controllers/diet/listDiets.js
+++ src/controllers/diet/listDiets.js
@@ -2,6 +2,21 @@ const Diet = require('../../models/diet');
 
 async function listDiets (req, res) {
     try {
+        if (filter.id) {
+            if (filter.id <= 0) {
+                return res.status(400).json({ message: 'O ID do paciente deve ser um número positivo' });
+            }
+
+            const diets = await Diet.findAll(
+                { where: { id_patient: filter.id } }
+            );
+
+            if (diets.length === 0) {
+                return res.status(400).json({ message: 'Paciente não possui dietas cadastradas' });
+            }
+
+            return res.status(200).json(diets);
+        }
         
         const diets = await Diet.findAll();
         return res.status(200).json(diets);
