eaea0f263dcd6fc71124add94ba5f1acd6e617f8
Reinaldo Porto <reinaldoportoti@gmail.com>
2023-10-31 21:00:28 -0300

    delete de paciente ou usuário desde que não haja nenhum vinculo

--- src/controllers/user/delUser.js
+++ src/controllers/user/delUser.js
@@ -1,17 +1,36 @@
 const { Sequelize } = require('sequelize');
 const User = require('../../models/user')
+const Patient = require('../../models/patient');
 
-const delUser = async(response, request)=>{
-    
-    const getUsuario = await User.findByPk(request.body.id)
-    if(getUsuario.id_type == 4){
-        const getBusca = await Sequelize.query(
-             `select users.id as user_id, exames.id as Exame_id from users
-             join exames on exames.id = users.id
-             join 
-             `
-            )
+const delUser = async (response, request) => {
+
+    const getUsuario = await User.findByPk(request.params.id)
+    const getPaciente = await User.findByPk(request.params.id_patient)
+    if (getUsuario.id_type !== 3 && getUsuario.id == request.params.id) {
+        const findId = await Patient.findOne({
+            include: {
+                all: true,
+                where: {
+                    id: getPaciente.id
+                }
+            }
+        })
+        if (findId) {
+            return response.status(400)
+                .json({
+                    message: ` Não é possível apagar um paciente caso tenha consulta, 
+                exame, medicação, dieta ou exercício cadastrado.
+            ` })
+        } else {
+            const result = await Patient.destroy({
+                where: {
+                    id: request.params.id
+                }
+            })
+            return response.status(201).json({ mensagem: "Excluido com sucesso." })
+        }
     }
+
 }
 
 module.exports = delUser;
