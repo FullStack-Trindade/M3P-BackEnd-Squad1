7c95f33704777106f85f6d7d6794d4c18759f7c8
Beatriz Christie <115591090+biachristie@users.noreply.github.com>
2023-11-04 11:17:55 -0300

    added find token by id user to resetPassword

--- src/controllers/password/resetPassword.js
+++ src/controllers/password/resetPassword.js
@@ -1,6 +1,15 @@
+const Token = require('../../models/token');
+
 async function sendNewPassword (req, res) {
     try {
+        const resetFunction = async(id_user, token, password) => {
+            let passwordResetToken = await Token.findOne({ 
+                where: {
+                    id_user: id_user 
+                }
+            })
 
+        }
     } catch (error) {
         return res.status(500).json({ message: 'Requisição não pode ser executada' });
     }
