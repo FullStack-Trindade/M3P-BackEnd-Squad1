45e99142f58627d25dece673315d6551fc6c2ed9
Beatriz Christie <115591090+biachristie@users.noreply.github.com>
2023-11-04 11:06:37 -0300

    added send email to resetPasswordRequest

--- src/controllers/password/resetPasswordRequest.js
+++ src/controllers/password/resetPasswordRequest.js
@@ -3,8 +3,10 @@ const bcrypt = require('bcrypt');
 
 const User = require('../../models/user');
 const Token = require('../../models/token');
+const sendEmail = require('../../utils/email/sendEmail');
 
 const bcryptSalt = process.env.BCRYPT_SALT;
+const port = process.env.SERVER_PORT;
 
 async function resetPasswordRequest (req, res) {
     try {
@@ -31,6 +33,17 @@ async function resetPasswordRequest (req, res) {
                 id_user: user.id,
                 token: hash,
             }).save();
+
+            const link = `localhost:${ port }/resetarSenha?token=${ resetToken }&id=${ user.id }`;
+        
+            sendEmail(
+                user.email, 
+                'Recuperação de senha', 
+                { name: user.name, link: link }, 
+                './template/requestResetPassword.handlebars'
+            )
+        
+            return link;
         }
     } catch (error) {
         return res.status(500).json({ message: 'Requisição não pode ser executada' });
