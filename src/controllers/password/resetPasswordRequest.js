755b96c2cc904903e09f101f8c7c1b627d99f34e
Beatriz Christie <115591090+biachristie@users.noreply.github.com>
2023-11-04 11:01:17 -0300

    added error status to resetPasswordRequest

--- src/controllers/password/resetPasswordRequest.js
+++ src/controllers/password/resetPasswordRequest.js
@@ -1,5 +1,9 @@
 async function resetPasswordRequest (req, res) {
-    
+    try {
+        
+    } catch (error) {
+        return res.status(500).json({ message: 'Requisição não pode ser executada' });
+    }
 }
 
 module.exports = resetPasswordRequest;
