c9859da3865eca805c46f54176c1bd7fb94d705c
gabrieldelpacheco <gabriel.del.pacheco@gmail.com>
2023-11-04 14:41:41 -0300

    correção completa no arquivo para criar exercicios.

--- src/controllers/exercise/createExercise.js
+++ src/controllers/exercise/createExercise.js
@@ -1,38 +1,48 @@
 const Patient = require('../../models/patient');
-const User = require('../../models/user');
 const Exercise = require('../../models/exercise');
 
-async function createExercise (request, response) {
+async function createExercise(request, response) {
+
     try {
-        const patient = await Patient.findByPk(request.body.id_patient);
-        const nurse = await User.findByPk(request.body.id_nurse);
+        const newDate = new Date()
+    const exercicioTime = `${newDate.getHours()}:${newDate.getMinutes()}:${newDate.getSeconds()}`
 
-        if (!patient) {
-            return response.status(400).json({ message: 'Paciente não consta nos registros' });
-        }
 
-        if (!nurse || nurse.id_type !== 2) {
-            return response.status(400).json({ message: 'Enfermeiro não consta nos registros' });
-        }
-
-        const dataExercise = {
-            id_patient: patient.id,
-            id_nurse: nurse.id,
-            seriesName: request.body.seriesName,
-            dateExercise: request.body.dateExercise,
-            hourExercise: request.body.hourExercise,
-            typeExercise: request.body.typeExercise,
-            amountWeek: request.body.amountWeek,
-            descritionExercise: request.body.descritionExercise,
-            status: request.body.status,
-        };
-
-        const newExercise = await Exercise.create(dataExercise);
-        return response.status(201).json(newExercise);
-    
-    } catch (error) {
-        return response.status(500).json({ message: 'Requisição não pode ser executada' });
+    const serieExercicio = {
+        id_patient: request.body.id_patient,
+/*         id_doctor: request.body.id_doctor, */
+        seriesName: request.body.seriesName,
+        dateExercise: request.body.dateExercise || newDate.setDate(newDate.getDate()),
+        hourExercise: request.body.hourExercise || examTime,
+        typeExercise: request.body.typeExercise,
+        amountWeek: request.body.amountWeek,
+        descritionExercise: request.body.descritionExercise,
     }
-}
+
+
+    const serieExisting = await Exercise.findOne({
+        where: {
+         /*    id_patient: exercicio.id_patient, */
+            seriesName: serieExercicio.seriesName,
+            dateExercise: serieExercicio.dateExercise,
+            hourExercise: serieExercicio.hourExercise
+        },
+    });
+
+    if (!serieExisting) {
+        const newSerie = await Exercise.create(serieExercicio);
+        response.status(201).json(newSerie)
+
+    } else {
+        return response.status(400).json({ message: 'Já possui serie de exercicio' })
+    }
+        
+    } catch (error) {
+        console.log(error);
+        return response.status(500).json({ message: 'Não foi possível atender sua solicitação' })
+    }
+    
+
+};
 
 module.exports = createExercise;
