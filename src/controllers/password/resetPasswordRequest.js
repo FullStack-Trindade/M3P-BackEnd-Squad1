0ea359e3eaf03d576930b34e0af62d16ad7d293f
Beatriz Christie <115591090+biachristie@users.noreply.github.com>
2023-11-04 11:05:49 -0300

    added create new token to resetPasswordRequest

--- src/controllers/password/resetPasswordRequest.js
+++ src/controllers/password/resetPasswordRequest.js
@@ -1,5 +1,10 @@
+const crypto = require('crypto');
+const bcrypt = require('bcrypt');
+
 const User = require('../../models/user');
 const Token = require('../../models/token');
+
+const bcryptSalt = process.env.BCRYPT_SALT;
 
 async function resetPasswordRequest (req, res) {
     try {
@@ -17,6 +22,15 @@ async function resetPasswordRequest (req, res) {
             });
 
             if(token) { await token.destroy() }
+
+            let resetToken = crypto.randomBytes(32).toString('hex');
+            
+            const hash = await bcrypt.hash(resetToken, Number(bcryptSalt));
+            
+            await new Token({
+                id_user: user.id,
+                token: hash,
+            }).save();
         }
     } catch (error) {
         return res.status(500).json({ message: 'Requisição não pode ser executada' });
