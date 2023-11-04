8f8073ad9fdced938be9013547f98f85903608ea
Beatriz Christie <115591090+biachristie@users.noreply.github.com>
2023-11-04 11:03:11 -0300

    added find user by email to resetPasswordRequest

--- src/controllers/password/resetPasswordRequest.js
+++ src/controllers/password/resetPasswordRequest.js
@@ -1,6 +1,15 @@
+const User = require('../../models/user');
+
 async function resetPasswordRequest (req, res) {
     try {
-        
+        const requestFunction = async(email) => {
+            const user = await User.findOne({ 
+                where: {
+                    email: email
+                }
+            });
+            
+        }
     } catch (error) {
         return res.status(500).json({ message: 'Requisição não pode ser executada' });
     }
