3b85f9781d2a377cf201bef3d906bfa3f14447b3
Beatriz Christie <115591090+biachristie@users.noreply.github.com>
2023-11-05 14:21:00 -0300

    changed connection at medication model

--- src/models/medication.js
+++ src/models/medication.js
@@ -1,7 +1,8 @@
-const connection = require("../database/index");
-const { Sequelize } = require("sequelize");
+const { Sequelize } = require('sequelize');
+const connection = require('../config/database');
+const sequelize = new Sequelize(connection);
 
-const Medication = connection.define("medication", {
+const Medication = sequelize.define("medications", {
   id: {
     type: Sequelize.INTEGER,
     autoIncrement: true,
