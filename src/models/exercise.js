138a9dc83912bd940a17aedb4a70e633642c21b5
gabrieldelpacheco <gabriel.del.pacheco@gmail.com>
2023-11-05 14:24:24 -0300

    faltava created_at e updated_at na tabela exercise

--- src/models/exercise.js
+++ src/models/exercise.js
@@ -50,6 +50,16 @@ const Exercise = connection.define('exercise',{
         allowNull: false,
         defaultValue: true,
     },
+    created_at: {
+        type: 'TIMESTAMP',
+        allowNull: false,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+    },
+    updated_at: {
+        type: 'TIMESTAMP',
+        allowNull: false,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+    }
 });
 
 module.exports = Exercise;
