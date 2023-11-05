4f618f83b9d9209b8ba7c6700d7f9e171da4351b
Daniel Simoni Wegher <w.daniels@hotmail.com>
2023-11-05 19:48:15 -0300

    Swwager

--- src/routes/login.js
+++ src/routes/login.js
@@ -3,6 +3,43 @@ const { Router } = require('express');
 // CONTROLLERS
 const login = require("../controllers/session/login");
 
+/**
+ * @swagger
+ * /api/usuarios/login:
+ *   post:
+ *     tags:
+ *       - Autenticação
+ *     summary: Endpoint para a liberação do Login do Usuário.
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               E-mail:
+ *                 type: string
+ *                 description: E-mail do usuário.
+ *                 example: exemplo@email.com
+ *               Senha:
+ *                 type: string
+ *                 description: Senha do usuário.
+ *                 example: senha123
+ *     responses:
+ *       200:
+ *         description: OK
+ *       400:
+ *         description: Bad Request
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   description: Mensagem de erro explicativa.
+ *                   example: Dados inválidos.
+ */
 // ROUTES
 const loginRoute = new Router();
 
